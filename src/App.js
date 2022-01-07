d327ed8eb0e6d4ecc8e34a5c8417aec77e214465
Tameem Hossain <t23hossa@uwaterloo.ca>
2022-01-07 13:38:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from "axios";
 import './App.css';
 import './index.js';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      playerName: null,
+      playerStats: {}
+    }
+  }
 
-  return (
-    <div className="App">      
-      <div> 
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-extra" href="https://www.nba.com/" target="_blank"> My favorite site </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Tameem and Tanisha
-        </a>
-      </header>
+  getPlayerId=()=>{
+    axios.get("https://www.balldontlie.io/api/v1/players?search=lebron")
+    .then(async res=>{
+      console.log(res.data.data)
+    }).catch(err=> {
+      console.log(err)
+    })
+  }
+
+  getPlayerStats = () => {
+    axios.get("https://www.balldontlie.io/api/v1/season_average?season=2019%player_ids[]=237")
+    .then(async res => {
+      console.log(res.data.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  componentDidMount() {
+    this.getPlayerId()
+  }
+
+  render() {
+    return ( 
+      <div className="App">      
+        
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
