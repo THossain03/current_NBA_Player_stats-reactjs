f8e9cadff6ef70ba4912a339fcb63974d843624d
Tameem Hossain <t23hossa@uwaterloo.ca>
2022-01-08 14:09:02 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -8,36 +8,80 @@ class App extends Component {
     super(props)
     this.state={
       playerName: null,
+      standings: {},
       playerStats: {}
     }
   }
 
-  getPlayerId=()=>{
-    axios.get("https://www.balldontlie.io/api/v1/players?search=lebron")
-    .then(async res=>{
-      console.log(res.data.data)
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getPlayerId()
+    console.log(this.state.playerName)
+  }
+
+  handleChange = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if (replace.length>=0) {
+      this.setState({playerName: replace}) 
+    }
+  }
+
+  getStandings=()=>{
+    axios.get("http://data.nba.net/10s/prod/v1/current/standings_conference.json")
+    .then(async res=> {
+      console.log(res.league.standard.conference.east.data)
+      this.setState({standings: res.data.league.standard.conference.east[0]})
     }).catch(err=> {
       console.log(err)
     })
   }
 
-  getPlayerStats = () => {
-    axios.get("https://www.balldontlie.io/api/v1/season_average?season=2019%player_ids[]=237")
+  getPlayerId=()=>{
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+    .then(async res=>{
+      console.log(res.data.data)
+      if(res.data.data[0] === undefined){
+        alert("This player's data is unavailable.")
+      } else if(res.data.data.length > 1) {
+        alert("Please specify the name more!")
+      } else {
+        await this.getPlayerStats(res.data.data[0].id)
+      }
+    }).catch(err=> {
+      console.log(err)
+    })
+  }
+
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?&player_ids[]=${playerId}`)
     .then(async res => {
       console.log(res.data.data)
+      this.setState({playerStats: res.data.data[0]})
     }).catch(err => {
       console.log(err)
     })
   }
 
-  componentDidMount() {
-    this.getPlayerId()
-  }
-
   render() {
     return ( 
       <div className="App">      
-        
+        <form onSubmit={this.handleSubmit}>
+         <label>
+          Name 
+          <input type="text" value={this.state.value} onChange={this.handleChange} placeholder='Enter Player Name'/> 
+        </label> 
+        <input type="submit" value="Submit"/>
+        </form>
+        Player: {this.state.playerName}
+        <br/>
+        Games Played: {this.state.playerStats["games_played"]}
+        <br/>
+        Points: {this.state.playerStats["pts"]}
+        <br/>
+        Rebounds: {this.state.playerStats["reb"]}
+        <br/>
+        Assists: {this.state.playerStats["ast"]}
+        <br/>
       </div>
     );
   }
